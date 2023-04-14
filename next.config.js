ccee47d3a38eb96f21c8c929b52d1c1bba8c8d14
Mitul Mohammad Abdullah Al Mukit <1155165211@link.cuhk.edu.hk>
2023-04-14 16:04:11 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'www.sksales.com',
+    ]
+  }
 }
 
 module.exports = nextConfig
